3ca72d53f7ac6194b0b3b46f392cacaef409b0f6
neuromagus <neuromagus@mail.ru>
2024-10-18 00:49:43 +0300

    update styles of Navbar (todo: disable reloading page on click search button)

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,17 @@ import "./header.css"
 export const Header = () => {
     return (
         <>
-            <header className="header-app">NEWS & BLOGS</header>
+            <header className="header">
+                <h1 className="logo">News & Blogs</h1>
+                <div className="search-bar">
+                    <form>
+                        <input type="text" placeholder="Search news" />
+                        <button type="submit">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                </div>
+            </header>
         </>
     )
 }
