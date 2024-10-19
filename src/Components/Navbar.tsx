4d7d98093ce808637638e63bd25d939c77a4ea39
neuromagus <neuromagus@mail.ru>
2024-10-19 19:45:50 +0300

    move images to the right place and create a content for left side

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,8 +3,25 @@ import "./navbar.css"
 export const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="user">User</div>
-            <nav className="categories">Categories</nav>
+            <div className="user">
+                <img alt="user image" src="/public/images/user.jpg" />
+                <p>Marys blog</p>
+            </div>
+            <nav className="categories">
+                <h1 className="nav-heading">Categories</h1>
+                <div className="nav-links">
+                    <a href="#" className="navlink">General</a>
+                    <a href="#" className="navlink">World</a>
+                    <a href="#" className="navlink">Bussiness</a>
+                    <a href="#" className="navlink">Technology</a>
+                    <a href="#" className="navlink">Entertainment</a>
+                    <a href="#" className="navlink">Sports</a>
+                    <a href="#" className="navlink">Science</a>
+                    <a href="#" className="navlink">Health</a>
+                    <a href="#" className="navlink">Nation</a>
+                    <a href="#" className="navlink">Bookmarks <i className="fa-regular fa-bookmark"></i></a>
+                </div>
+            </nav>
         </div>
     )
 }
